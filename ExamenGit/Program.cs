42d61f186e376b8d9c5da6e77fe10f474b719085
MrDimaBahmetev <bahmetev168@gmail.com>
2021-11-24 21:57:40 +0600

    make full project (Find gap)

---
+++ ExamenGit/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ExamenGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                try
+                {
+                    Menu();
+                    if (Convert.ToInt16(UserChoice()) == 1)
+                    {
+                        ShowGap(FindGap(EnterPoint(), EnterPoint()));
+                    }
+                    else
+                        break;
+                }
+                catch (Exception e)
+                {
+                    if (e.Message == "Input string was not in a correct format.")
+                        break;
+                }
+
+            } while (true);
+            Console.ReadKey();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Вычислить промежуток");
+            Console.WriteLine("Клавиша. Выход");
+            Console.Write("Введите: ");
+        }
+        static string UserChoice()
+        {
+            return Console.ReadLine();
+        }
+        static void ShowEnterPoint()
+        {
+            Console.Write("Введите точку: ");
+        }
+
+        static decimal EnterPoint()
+        {
+            ShowEnterPoint();
+            return Convert.ToDecimal(Console.ReadLine());
+        }
+        static void ShowGap(decimal gap)
+        {
+            Console.WriteLine("Промежуток: "+gap);
+            Console.ReadKey();
+        }
+        static decimal FindGap(decimal pointA, decimal pointB)
+        {
+            if(pointA < pointB)
+            {
+                return pointB - pointA;
+            }
+            else
+            {
+                return pointA - pointB;
+            }
+        }
+    }
+}
